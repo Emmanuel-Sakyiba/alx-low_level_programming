e53375bb1784a5997976a14f81b2c836c9d414ee
Emmanuel-Sakyiba <manuelbusinesspro3185@gmail.com>
2023-06-22 23:00:33 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,6 @@
 
 /**
  * print_numbers - print 0-9
- * @c: char to print_numbers
- *
  * Return: void
  */
 
@@ -12,7 +10,7 @@ void print_numbers(void)
 {
 	char c;
 
-	for(c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
