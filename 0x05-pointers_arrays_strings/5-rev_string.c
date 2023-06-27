2c8e5b97819458a2a0c5f1ce4379eb04f8226b08
Emmanuel-Sakyiba <manuelbusinesspro3185@gmail.com>
2023-06-27 13:46:21 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * rev_strings - Reserves a string
+ * @s: input_reserve_string
+ * Return: strings in a reserve
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
