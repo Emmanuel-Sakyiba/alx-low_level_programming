8e2f5678afaa05d132861ceaf66740293f1f35bf
Emmanuel-Sakyiba <manuelbusinesspro3185@gmai.com>
2023-07-03 20:45:31 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,19 +2,19 @@
 
 
 /**
- * _strchr - Entry point
+ * _strchr - a function that locates a character in a string
  * @s: input
  * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-	int j;
+	int i = 0;
 
-	for (; s[j] >= '\0'; j++)
+	for (; s[i] >= '\0'; i++)
 	{
-		if (s[j] == c)
-			return (&s[j]);
+		if (s[i] == c)
+			return (&s[i]);
 	}
 	return (0);
 }
