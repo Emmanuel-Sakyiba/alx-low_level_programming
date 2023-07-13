e2860b590b31b4111904627457b4847369e3cb94
Emmanuel-Sakyiba <manuelbusinesspro3185@gmai.com>
2023-07-13 12:09:21 -0700

    work -2-calloc -project

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
+
+
 	while (s1 && s1[len1])
 
 		len1++;
@@ -28,9 +30,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len2)
 
 		s = malloc(sizeof(char) * (len1 + n + 1));
+
 	else
 
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
 
 	if (!s)
 
@@ -49,10 +53,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (n < len2 && i < (len1 + n))
 
 		s[i++] = s2[j++];
-
-
+	
 	while (n >= len2 && i < (len1 + len2))
+
 		s[i++] = s2[j++];
+
 	s[i] = '\0';
+
 	return (s);
 }
